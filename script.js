2cc60e7ad69a1d3c1fd995f10768d67d5d5a77a7
Inda Smith <inda.k.smith@gmail.com>
2023-03-11 14:01:03 -0500

    added text to html, centered and changed color with css

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "You can call me Katie though.";
 document.querySelector("body").appendChild(h2);
