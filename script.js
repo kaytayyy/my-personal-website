1df1439286395836b4acae2a88a7e58f41ed6a0b
Inda Smith <inda.k.smith@gmail.com>
2023-03-11 17:38:11 -0500

    made changes to javascript and added a random quote generator

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "You can call me Katie though.";
-document.querySelector("body").appendChild(h2);
-
-const myBtn = document.getElementById("myBtn"); {
-myBtn.addEventListener("click") 
-window.alert("Woah it actually did something!")
-};
+writeRandomQuote = function () {
+    var quotes = new Array();
+    quotes[0] = "Please consider me for future jobs.";
+    quotes[1] = "I promise I am smart.";
+    quotes[2] = "I crochet sometimes.";
+    quotes[3] = "Look at my pets.";
+    quotes[4] = "Krusty Krab pizza.";
+    var rand = Math.floor(Math.random()*quotes.length);
+    document.write(quotes[rand]);
+  }
+  writeRandomQuote();
