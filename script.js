5e38f4978a35af9192400fbea5a8b555e2c60256
Inda Smith <inda.k.smith@gmail.com>
2023-03-11 16:47:43 -0500

    made button adjustments and added images.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "You can call me Katie though.";
 document.querySelector("body").appendChild(h2);
+
+const myBtn = document.getElementById("myBtn"); {
+myBtn.addEventListener("click") 
+window.alert("Woah it actually did something!")
+};
